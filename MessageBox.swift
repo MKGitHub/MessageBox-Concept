8f0e1ada7c5204fb00b31371b797c8f81d1a9b82
Mohsan Khan <git.mk@xybernic.com>
2016-07-03 13:27:30 +0200

    Updated for Swift 3.

--- MessageBox.swift
+++ MessageBox.swift
@@ -1,12 +1,34 @@
 //
 //  MessageBox.swift
-//  version 1.0.0
+//  version 1.0.2
 //
 //  Created by Mohsan Khan on 2016-03-27.
 //  Copyright © 2016 Mohsan Khan. All rights reserved.
 //
 
+/*
+    GitHub Notes
+    Swift Version: 3 beta 1
+    URL: https://github.com/MKGitHub/MessageBox-Concept
+*/
+
 import Foundation
+
+
+/*
+    Example Usage:
+
+    private let mMessageBox:MessageBox = MessageBox()
+ 
+     // put message
+     mMessageBox.set(object:"TestObject1", key:"TestKey1")
+
+     // get message, don't remove
+     let someObject:String = mMessageBox.get(objectForKey:"TestKey1", removeObject:no)
+
+     // get message, remove
+     let someObject:String = mMessageBox.get(objectForKey:"TestKey1", removeObject:yes)
+*/
 
 
 final class MessageBox
@@ -15,35 +37,25 @@ final class MessageBox
     private var mMessageDictionary:Dictionary<String, AnyObject> = Dictionary<String, AnyObject>()
 
 
-    // MARK:- Life Cycle
-
-
-    /*init()
-    {
-    }*/
-
-
-    /*deinit
-    {
-    }*/
-
-
     // MARK:- Public Methods
 
 
-    func setObject(obj:AnyObject, key:String)
+    func set(object:AnyObject, key:String)
     {
-        mMessageDictionary[key] = obj
+        mMessageDictionary[key] = object
     }
 
 
-    func getObject(key:String, removeObject:Bool)
-    -> AnyObject
+    ///
+    /// Returns `nil` if the object does not exist.
+    ///
+    func get(objectForKey key:String, removeObject:Bool)
+    -> AnyObject?
     {
-        let obj:AnyObject = mMessageDictionary[key]!
+        let obj:AnyObject? = mMessageDictionary[key]
 
         if (removeObject) {
-            mMessageDictionary.removeValueForKey(key)
+            mMessageDictionary.removeValue(forKey:key)
         }
 
         return obj
